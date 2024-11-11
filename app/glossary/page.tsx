65cd916e4ca84dc0a13e2c433fd7b1a6d99cc57e
timuroutdoor <zaltsani1@gmail.com>
2024-11-11 16:53:18 +0700

    Add Glossary

--- app/glossary/page.tsx
+++ app/glossary/page.tsx
@@ -1,21 +1,18 @@
 'use client'
 
 import { CardContent } from "@mui/material"
-import { Card, CardTitle } from "app/ui/Cards"
+import { Card, CardFooter, CardTitle } from "app/ui/Cards"
 import { useState } from "react"
+import glossaryData from 'app/glossary/glossary-data.json'
+import siteMetadata from '@/data/siteMetadata'
+import Link from "next/link"
 
 
 export default function Page() {
-    const [elements, setElements] = useState([
-        { title: 'Shooting', visibility: false},
-        { title: 'Passing', visibility: false},
-        { title: 'Crossing', visibility: false},
-        { title: 'Shooting On Target', visibility: false},
-        { title: 'Shooting 1', visibility: false},
-        { title: 'Shooting 2', visibility: false},
-        { title: 'Shooting 3', visibility: false},
-        { title: 'Shooting 4', visibility: false},
-    ])
+    const [elements, setElements] = useState(glossaryData.map(d => ({
+        ...d,
+        visibility: true
+    })))
     const toggleVisibility = (title) => {
         const newState = elements.map((element) =>
             element.title === title
@@ -34,23 +31,37 @@ export default function Page() {
             </div>
             <div className="grid md:grid-cols-2 grid-cols-1 grid-flow-row auto-rows gap-6 text-gray-900 dark:text-white">
                 {elements.map(element => (
-                    <Card key={element.title} className={`p-3 ${element.visibility ? 'row-auto' : 'row-span-1'}`} >
+                    <Card key={element.title} className={`p-3 ${element.visibility ? 'row-auto' : 'row-auto'}`} >
                         <CardTitle className="flex justify-between p-3">
-                            <button onClick={() => toggleVisibility(element.title)}>
+                            <button
+                                // onClick={() => toggleVisibility(element.title)}
+                            >
                                 {element.title}
                             </button>
-                            <button onClick={() => toggleVisibility(element.title)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 justify-end">
+                            <button
+                                // onClick={() => toggleVisibility(element.title)}
+                            >
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 justify-end">
                                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
-                                </svg>
+                                </svg> */}
                             </button>
                         </CardTitle>
                         
                         {/* Element content */}
                         {element.visibility && (
-                            <CardContent>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequatur provident voluptate, fugiat commodi eaque qui quae explicabo? Itaque rerum corporis nemo minima eum? Laboriosam unde incidunt quibusdam deleniti maiores!
-                            </CardContent>
+                            <>
+                                <CardContent>
+                                    {element.description}
+                                </CardContent>
+                                {element.url && (
+                                    <CardFooter>
+                                        <Link
+                                        href={siteMetadata.siteUrl + element.url}
+                                        className="text-blue-500"
+                                        >Read More</Link>
+                                    </CardFooter>
+                                )}
+                            </>
                         )}
                     </Card>
                 ))}
